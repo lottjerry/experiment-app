5f7a764b720ca896576c250d6d81fd417575fa5c
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-04-12 12:51:31 -0500

    first commit

---
+++ client/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+import MessagesView from '../views/MessagesView.vue';
+import ProfileView from '../views/ProfileView.vue';
+import store from '../store/index.js'
+
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: '/',
+		name: 'login',
+		component: LoginView,
+	},
+	{
+		path: '/signup',
+		name: 'signup',
+		// route level code-splitting
+		// this generates a separate chunk (signup.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "signup" */ '../views/SignupView.vue'),
+	},
+	{
+		path: '/messages',
+		name: 'messages',
+		component: MessagesView,
+		beforeEnter(to, from, next) {
+			//console.log(store);
+			store
+				.dispatch('auth/authenticate')
+				.then(() => {
+					next();
+				})
+				.catch(() => {
+					next('/');
+				});
+		},
+	},
+
+	{
+		path: '/profile',
+		name: 'profile',
+		component: ProfileView,
+		beforeEnter(to, from, next) {
+			//console.log(store);
+			store
+				.dispatch('auth/authenticate')
+				.then(() => {
+					next();
+				})
+				.catch(() => {
+					next('/');
+				});
+		},
+	},
+];
+
+const router = new VueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
+});
+
+export default router;
