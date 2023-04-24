08ce839200f05b09e5fc9200d07b33b59dce6a8d
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-04-24 13:06:09 -0500

    Added combo box that selects a user and outputs users data

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h1 class="text-2xl text-blue-600 font-bold">Admin</h1>
+    <ComboBox />
   </div>
 </template>
 
 <script>
+import ComboBox from '../components/ComboBox.vue'
+
   export default {
-    
+    components: {
+      ComboBox
+    }
   }
 </script>
 
